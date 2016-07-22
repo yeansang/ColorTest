cc1af9fb6ca330250d7782fbe37586a302ca840b
yeansang ahn <ysan1991@gmail.com>
2016-07-22 15:17:56 +0900

    first commit

---
+++ app/src/main/java/com/example/nemus/colortest/ColorCal.java
@@ -0,0 +1,37 @@
+package com.example.nemus.colortest;
+
+import android.graphics.ColorMatrix;
+
+/**
+ * Created by nemus on 2016-07-22.
+ */
+public class ColorCal {
+
+
+    public static ColorMatrix adjustHue(float value)
+    {
+        value = cleanValue(value, 180f) / 180f * (float) Math.PI;
+        if (value == 0)
+        {
+            return null;
+        }
+        float cosVal = (float) Math.cos(value);
+        float sinVal = (float) Math.sin(value);
+        float lumR = 0.213f;
+        float lumG = 0.715f;
+        float lumB = 0.072f;
+        float[] mat = new float[]
+                {
+                        lumR + cosVal * (1 - lumR) + sinVal * (-lumR), lumG + cosVal * (-lumG) + sinVal * (-lumG), lumB + cosVal * (-lumB) + sinVal * (1 - lumB), 0, 0,
+                        lumR + cosVal * (-lumR) + sinVal * (0.143f), lumG + cosVal * (1 - lumG) + sinVal * (0.140f), lumB + cosVal * (-lumB) + sinVal * (-0.283f), 0, 0,
+                        lumR + cosVal * (-lumR) + sinVal * (-(1 - lumR)), lumG + cosVal * (-lumG) + sinVal * (lumG), lumB + cosVal * (1 - lumB) + sinVal * (lumB), 0, 0,
+                        0f, 0f, 0f, 1f, 0f,
+                        0f, 0f, 0f, 0f, 1f };
+        return new ColorMatrix(mat);
+    }
+
+    protected static float cleanValue(float p_val, float p_limit)
+    {
+        return Math.min(p_limit, Math.max(-p_limit, p_val));
+    }
+}
---
+++ app/src/main/java/com/example/nemus/colortest/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.nemus.colortest;
+
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar redSeek;
+    private SeekBar greenSeek;
+    private SeekBar blueSeek;
+    private TextView redText;
+    private TextView greenText;
+    private TextView blueText;
+
+    private ImageView imageView;
+
+    float[] mat = new float[]
+            {
+                    1f, 0f, 0f, 0, 0,
+                    0f, 1f, 0f, 0, 0,
+                    0f, 0f, 1f, 0, 0,
+                    0f, 0f, 0f, 1, 0};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        redSeek = (SeekBar)findViewById(R.id.red_seek);
+        greenSeek = (SeekBar)findViewById(R.id.green_seek);
+        blueSeek = (SeekBar)findViewById(R.id.blue_seek);
+        redText = (TextView)findViewById(R.id.red_text);
+        greenText = (TextView)findViewById(R.id.green_text);
+        blueText = (TextView)findViewById(R.id.blue_text);
+
+        imageView = (ImageView)findViewById(R.id.image);
+
+        float[] mat1 = new float[]
+                {
+                        0.3f, 0.3f, 0.3f, 0, 0,
+                        0.3f, 0.3f, 0.3f, 0, 0,
+                        0.3f, 0.3f, 0.3f, 0, 0,
+                        0f, 0f, 0f, 1, 0};
+        ColorFilter cff = imageView.getColorFilter();
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(mat1);
+        imageView.setColorFilter(cf);
+
+        redSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                float val = (float)i/100;
+                float val2 = (1f-val)/2;
+                redText.setText(val+"");
+                mat[0] = val;
+                mat[1] = val2;
+                mat[2] = val2;
+                /*float[] mat = new float[]
+                        {
+                                val, val2, val2, 0, 0,
+                                0f, 1f, 0f, 0, 0,
+                                0f, 0f, 1f, 0, 0,
+                                0f, 0f, 0f, 1, 0};*/
+                ColorFilter cff = imageView.getColorFilter();
+                ColorMatrixColorFilter cf = new ColorMatrixColorFilter(mat);
+                imageView.setColorFilter(cf);
+                //imageView.clearColorFilter();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        greenSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                greenText.setText(i+"");
+                float val = (float)i/100;
+                float val2 = (1f-val)/2;
+                mat[5] = val2;
+                mat[6] = val;
+                mat[7] = val2;
+                /*float[] mat = new float[]
+                        {
+                                1f, 0f, 0f, 0, 0,
+                                val2, val, val2, 0, 0,
+                                0f, 0f, 1f, 0, 0,
+                                0f, 0f, 0f, 1, 0};*/
+                ColorFilter cff = imageView.getColorFilter();
+                ColorMatrixColorFilter cf = new ColorMatrixColorFilter(mat);
+                imageView.setColorFilter(cf);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        blueSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                blueText.setText(i+"");
+                float val = (float)i/100;
+                float val2 = (1f-val)/2;
+                mat[10] = val2;
+                mat[11] = val2;
+                mat[12] = val;
+                /*float[] mat = new float[]
+                        {
+                                1f, 0f, 0f, 0, 0,
+                                0f, 1f, 0f, 0, 0,
+                                val2, val2, val, 0, 0,
+                                0f, 0f, 0f, 1, 0};*/
+                ColorMatrixColorFilter cf = new ColorMatrixColorFilter(mat);
+                imageView.setColorFilter(cf);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+}
